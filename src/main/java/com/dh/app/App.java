7766d7126aa806e0edbfd45c23d14e7c70c5d279
dhughes <dhughes2@statestreet.com>
2019-04-26 00:22:18 +0100

    basic kstream examples added for record type string and avro

--- src/main/java/com/dh/app/App.java
+++ src/main/java/com/dh/app/App.java
@@ -1,7 +1,11 @@
 package com.dh.app;
 
 import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde;
+
+import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KafkaStreams;
@@ -27,20 +31,33 @@ public class App {
 
         System.out.println("Start: kafka-streams-example");
         
-        final Properties streamsConfiguration = new Properties();
-        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-streams-example");
-        streamsConfiguration.put(StreamsConfig.CLIENT_ID_CONFIG, "kafka-streams-example-client");
-        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        streamsConfiguration.put(StreamsConfig.STATE_DIR_CONFIG, "STATE_DIR");
-        streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        final Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-streams-example");
+        props.put(StreamsConfig.CLIENT_ID_CONFIG, "kafka-streams-example-client1");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+		props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+		props.put(StreamsConfig.STATE_DIR_CONFIG, "STATE_DIR");
+		props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         final StreamsBuilder builder = new StreamsBuilder();
 
-        final GlobalKTable<String, String> table = builder.globalTable("basic-java-producer-topic",
+        // *GlobalKTable Example*
+        final GlobalKTable<String, String> table = builder.globalTable("basic-java-producer-topic123",
                 Materialized.<String, String, KeyValueStore<Bytes, byte[]>>as("TABLE_STORE")
                         .withKeySerde(Serdes.String()).withValueSerde(Serdes.String()));
         
-        final KafkaStreams streams = new KafkaStreams(builder.build(), streamsConfiguration);
+        // *Stream Join Example*
+        final KStream<String, String> basic_java_producer_stream = builder.stream("basic-java-producer-topic", Consumed.with(Serdes.String(), Serdes.String()));
+        final Map<String, String> serdeConfig = Collections.singletonMap("schema.registry.url", "http://localhost:8081");
+        final Serde<GenericRecord> valueGenericAvroSerde = new GenericAvroSerde();
+        valueGenericAvroSerde.configure(serdeConfig, false); // `false` for record values
+        final KStream<String, GenericRecord> avro_java_producer_stream = builder.stream("avro-java-producer-topic", Consumed.with(Serdes.String(), valueGenericAvroSerde));
+        
+
+//        final KStream<String, GenericRecord> sjoin = basic_java_producer_stream.join(otherStream, joiner, windows, joined)
+        
+        final KafkaStreams streams = new KafkaStreams(builder.build(), props);
         streams.cleanUp();
         streams.start();
 
